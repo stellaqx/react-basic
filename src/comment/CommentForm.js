413ff30e4d735eacfe8d8d08cae56399a3d494ca
Qian <ee.xuqian@gmail.com>
2020-05-21 09:12:01 -0700

    bind the onSubmit handler function with self, and then extract the value from ref element.

--- src/comment/CommentForm.js
+++ src/comment/CommentForm.js
@@ -3,18 +3,22 @@ import React from 'react';
 class CommentForm extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
-        console.log("submitted form");
+        
+        let author = this.refs.author.value;
+        let text = this.refs.text.value;
+
+        console.log("submitted form", author, text);
     }
 
     render() {
         // JSX
         return (
-            <form className="ui reply form" onSubmit={this.handleSubmit}>
+            <form className="ui reply form" onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
-                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Name" ref="author" />
                 </div>
                 <div className="field">
-                    <textarea placeholder="Comment" />
+                    <textarea placeholder="Comment" ref="text" />
                 </div>
                 <button type="submit" className="ui blue button">
                     Add a comment.
