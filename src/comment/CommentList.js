52f0dba5c50fda17259c670e25607021b3aed0da
Qian <ee.xuqian@gmail.com>
2020-05-20 23:09:29 -0700

    Created comment list.

---
+++ src/comment/CommentList.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+class CommentList extends React.Component {
+    render() {
+        // JSX
+        return (
+            <div className="ui commentList">
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default CommentList;
